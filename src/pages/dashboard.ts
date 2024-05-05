bffeea768471ee1ec2ab84d6fc83037d8b47ecb5
Jeremy Hut <jhut98@gmail.com>
2024-05-05 21:34:31 +0200

    Dashboard animated

--- src/pages/dashboard.ts
+++ src/pages/dashboard.ts
@@ -10,27 +10,49 @@ socket.on("init-votes", (acts) => {
     actList = acts;
 
     document.querySelector(".votes").innerHTML =
-        Object.entries(actList).map(([key, act]) => "<tr>\
+        rankedActs()
+            .map(([key, act], index) => "<tr data-act='" + key + "' data-score='" + act.score + "' data-ranking='" + index + "'>\
                     <td>" + key + "</td>\
-                    <td>" + act.country + "</td>\
-                    <td>" + act.contestant + "</td>\
-                    <td>" + act.song + "</td>\
-                    <td><progress value='" + act.score + "' data-act='" + key + "'></progress></td>\
+                    <td><progress value='" + act.score + "'></progress>" + act.country + "</td>\
+                    <td>" + act.score + "</td>\
                 </tr>").join('');
 
     updateHighestScore();
+    positionRows();
 });
 
 socket.on("update-votes", (acts) => {
     actList = acts;
 
-    for (const [key, act] of Object.entries(actList)) {
-        const element: HTMLProgressElement = document.querySelector(".votes progress[data-act='" + key + "']");
-        element.value = act.score;
-    }
+    rankedActs().forEach(([key, value], index) => {
+        const rowElement: HTMLProgressElement = document.querySelector(".votes tr[data-act='" + key + "']");
+        rowElement.dataset.ranking = index.toString();
+        rowElement.dataset.score = value.score.toString();
+        (rowElement.querySelector(':last-child') as HTMLElement).innerText = value.score.toString();
+        (rowElement.querySelector('progress') as HTMLProgressElement).value = value.score;
+    });
 
     updateHighestScore();
+    positionRows();
 });
+
+const positionRows = () => {
+    document.querySelectorAll(".votes tr").forEach((element: HTMLElement) => {
+        const ranking = Number.parseInt(element.dataset.ranking);
+        const leftRowSize = Math.floor(Object.keys(actList).length / 2);
+        const rowHeight = 64;
+        let top = ranking * rowHeight;
+        let left = 0;
+
+        if (ranking > leftRowSize) {
+            top = (ranking - leftRowSize - 1) * rowHeight
+            left = 50.5;
+        }
+
+        element.style.top = top + 'px';
+        element.style.left = left + '%';
+    })
+}
 
 socket.on("update-people", (people) => {
     document.getElementById("people").innerHTML = people.map(person => "<li>" + person + "</li>").join('');
@@ -44,3 +66,5 @@ const updateHighestScore = () => {
 };
 
 const findHighestScore = () => Math.max(...Object.values(actList).map(act => act.score));
+
+const rankedActs = () => Object.entries(actList).sort(([, actA], [, actB]) => actB.score - actA.score);
